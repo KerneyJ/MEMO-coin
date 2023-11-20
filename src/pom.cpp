59de2e51d4141e2ac4ebfc7b8bb9dc5a6b3d7eb8
kerneyJ <gamerguy9801@gmail.com>
2023-11-20 17:41:12 -0600

    working pom

--- src/pom.cpp
+++ src/pom.cpp
@@ -7,8 +7,8 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
+#include "wallet.hpp"
 
-#include "blake3/blake3.h"
 #include "pom.hpp"
 
 bool check_solution(Blake3Hash result, int difficulty) {
@@ -20,23 +20,28 @@ bool check_solution(Blake3Hash result, int difficulty) {
     for(int i = 0; i < difficulty; i++) {
         for(int j = 0; j < 8; ++j)
             if(result[i] >> j & 1){
-                return correct >= diffculty;
+                return correct >= difficulty;
             } else {
                 correct++;
             }
     }
 
-    return correct >= diffculty;
+    return correct >= difficulty;
 }
 
 bool comphash(Blake3Hash h1, Blake3Hash h2){
-    uint64_t* h1buf = (uint64_t*)h1;
-    uint64_t* h2buf = (uint64_t*)h2;
+    uint8_t* h1buf = (uint8_t*)h1.data();
+    uint8_t* h2buf = (uint8_t*)h2.data();
     for(int i = 0; i < 4; i++){
         if(h1buf[i] < h2buf[i])
             return true;
     }
     return false;
+}
+
+ProofOfMemory::ProofOfMemory(struct Wallet wallet){
+    this->wallet = wallet;
+    this->gen_hashes();
 }
 
 void ProofOfMemory::gen_hashes(){
@@ -57,33 +62,52 @@ void ProofOfMemory::gen_hashes(){
 
         blake3_hasher_init(&hasher);
         blake3_hasher_update(&hasher, solution.data(), BLAKE3_OUT_LEN);
-        blake3_hasher_update(&hasher, /*public key*/, BLAKE3_OUT_LEN);
+        blake3_hasher_update(&hasher, this->wallet.pub_key.data(), BLAKE3_OUT_LEN);
         blake3_hasher_finalize(&hasher, result.data(), BLAKE3_OUT_LEN);
 
-        hashes.push_back(std::pair<Blake3Hash, Blake3Hash>(result, solution));
+        hashes[nonce] = std::pair<Blake3Hash, Blake3Hash>(result, solution);
+        printf("Generating %lu/%lu hashes\r", nonce, this->hashes.max_size());
         nonce++;
     }
+    printf("\n");
 
 }
 
 
 Blake3Hash ProofOfMemory::solve_hash(Blake3Hash prev_hash, int difficulty) {
+    blake3_hasher hasher;
+    Blake3Hash h, result;
     // for now search for hash linearly through the array
-    for(int i = 0; i < 
+    for(uint64_t i = 0; i < this->hashes.max_size(); i++){
+        if(i % 1000000 == 0)
+            printf("Solving hash ...\n");
+        h = this->hashes[i].first;
+        blake3_hasher_init(&hasher);
+        blake3_hasher_update(&hasher, prev_hash.data(), BLAKE3_OUT_LEN);
+        blake3_hasher_update(&hasher, h.data(), BLAKE3_OUT_LEN);
+        blake3_hasher_finalize(&hasher, result.data(), BLAKE3_OUT_LEN);
+        if(check_solution(result, difficulty)){
+            printf("Solved hash in %lu iteartions.\n", i);
+            return result;
+        }
+    }
+    result.fill(0);
+    return result;
 }
 
 int main(void){
     Blake3Hash prev_hash;
     prev_hash.fill(255);
 
-    IConsensusModel* validator = new ProofOfMemory();
+    struct Wallet wallet = create_wallet();
+    IConsensusModel* validator = new ProofOfMemory(wallet);
 
-    validator.gen_hashes();
-    Blake3Hash solution = validator->solve_hash(prev_hash, 26);
+    Blake3Hash solution = validator->solve_hash(prev_hash, 22);
 
     for(int i = 0; i < solution.size(); i++) {
         printf("%02x", solution[i]);
     }
     printf("\n");
+
     return 0;
 }
--- src/pom.hpp
+++ src/pom.hpp
@@ -1,9 +1,10 @@
 #include <array>
 #include <cstdint>
 
-#include "blake3/blake3.h"
+#include "../external/blake3/blake3.h"
 #include "defs.hpp"
 #include "consensus.hpp"
+#include "wallet.hpp"
 
 #pragma once
 
@@ -11,7 +12,9 @@ class ProofOfMemory : public IConsensusModel {
     public:
         Blake3Hash solve_hash(Blake3Hash hash, int difficulty);
         void gen_hashes();
+        ProofOfMemory(struct Wallet wallet);
 
     private:
-        std::array<std::pair<Blake3Hash, Blake3Hash>, 1<<30 / BLAKE3_OUT_LEN> hashes;
+        std::array<std::pair<Blake3Hash, Blake3Hash>, (1<<30) / BLAKE3_OUT_LEN> hashes;
+        struct Wallet wallet;
 };
