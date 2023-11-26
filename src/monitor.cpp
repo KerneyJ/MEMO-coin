e5fdc522b8b37f923685ee48f8eaa1d38c9ffa89
Harrison <jmohr1@hawk.iit.edu>
2023-11-26 16:35:12 -0600

    Added monitor.cpp with function headers

---
+++ src/monitor.cpp
@@ -0,0 +1,35 @@
+/* 
+Commands for getting info about the blockchain's state.
+*/
+
+#include "defs.hpp"
+#include "transaction.hpp"
+#include "wallet.hpp"
+#include "keys.hpp"
+#include "messages.hpp"
+
+//prints the contents of the last block of the blockchain.
+void last_block() {
+
+}
+
+// (optional) prints the kth block in the blockchain.
+void print_block(int k) {
+
+}
+
+//prints info about the unconfirmed transactions in the transaction pools.
+void unconf_trans() {
+
+}
+
+//prints the number of validators in the network.
+void num_validators() {
+
+}
+
+
+//prints statistics on the network size, especially as it relates to proof of memory and proof of space resources.
+void netspace() {
+
+}
