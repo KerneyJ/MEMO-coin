12762321e800fdf26dc58cbbf28c74e45194d6a1
Harrison <jmohr1@hawk.iit.edu>
2023-11-26 18:15:20 -0600

    More on monitor.cpp

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -13,23 +13,22 @@ void last_block() {
 
 }
 
-// (optional) prints the kth block in the blockchain.
-void print_block(int k) {
-
-}
 
 //prints info about the unconfirmed transactions in the transaction pools.
 void unconf_trans() {
-
+    //for every transaction in the transaction pool, if status is UNCONFIRMED, print transaction src,dest,signature,id,amount,timestamp,status
 }
-
 //prints the number of validators in the network.
 void num_validators() {
-
+    //metronome keeps track of the number of validators. Query the metronome.
 }
-
 
 //prints statistics on the network size, especially as it relates to proof of memory and proof of space resources.
 void netspace() {
+    //Metronome keeps track of the netspace. Query the metronome.
+}
+
+// (optional) prints the kth block in the blockchain.
+void print_block(int k) {
 
 }
