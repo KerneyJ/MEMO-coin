03caf854ae65b992d4cc6a1cad309016e7ffc03c
Ian Dougherty <ianedougherty01@gmail.com>
2023-11-26 18:02:15 -0600

    key hashing

--- src/keys.hpp
+++ src/keys.hpp
@@ -1,9 +1,26 @@
 #include <array>
+#include <functional>
 #include <string>
 #include <vector>
 #include <cstdint>
+#include <unordered_map>
 
 #include "defs.hpp"
+
+struct Ed25519KeyHash {
+    std::size_t operator () (Ed25519Key const &v) const
+    {
+        std::hash<uint64_t> hasher;
+        uint64_t* arr = (uint64_t*) v.data();
+
+        auto h1 = hasher(v[0]);
+        auto h2 = hasher(v[1]);
+        auto h3 = hasher(v[2]);
+        auto h4 = hasher(v[3]);
+
+        return h1 ^ h2 ^ h3 ^ h4;
+    }
+};
 
 void gen_keys_ed25519(Ed25519Key &pub_key, Ed25519Key &priv_key);
 Ed25519Signature sign_data_ed25519(Ed25519Key priv_key, uint8_t* data, size_t len);
