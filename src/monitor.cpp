397799dc98e4e24b9ad62e666a57eeec187f4c37
Harrison <jmohr1@hawk.iit.edu>
2023-11-28 19:56:52 -0600

    updated monitor

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -9,11 +9,13 @@ Commands for getting info about the blockchain's state.
 #include "messages.hpp"
 #include <zmq.h>
 #include "config.hpp"
+#include "utils.hpp"
 
 
 //prints the contents of the last block of the blockchain.
 void last_block() {
     printf("last_block not implemented");
+    printf("/n");
     return;
 }
 
@@ -34,36 +36,42 @@ void num_trans() {
     zmq_close(requester);
     zmq_ctx_destroy(context);
     printf("size of transation queue: %d transactions in queue", response.data);
+    printf("/n");
     return;
 }
 //prints the number of validators in the network.
 void num_validators() {
     //metronome keeps track of the number of validators. Query the metronome.
     printf("num_validators not implemented");
+    printf("/n");
     return;
 }
 
 //prints the number of wallet addresses
 void num_wallets() {
     printf("num_wallets  not implemented");
+    printf("/n");
     return;
 }
 
 //prints the total number of coins in circulation
 void num_coins() {
     printf("num_coins  not implemented");
+    printf("/n");
     return;
 }
 
 // prints the number of hashes per second
 void hashes_per_second() {
     printf("hashes_per_second not implemented");
+    printf("/n");
     return;
 }
 
 //prints the total number of hashes stored
 void hashes_stored() {
     printf("hashes_stored not implemented");
+    printf("/n");
     return;
 }
 
